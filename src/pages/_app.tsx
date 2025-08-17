df565cf382ebd9f9b856b33718f1c8d8e441a16e
oyuh <lawsonhart0919@gmail.com>
2025-08-16 22:06:47 -0500

    ana4

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,12 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Component {...pageProps} />
       <Analytics />
+      <Component {...pageProps} />
     </>
   );
 }
