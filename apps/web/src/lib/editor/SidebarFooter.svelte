3f99d6d864e944bf04284c4fba70ca4ba1e89c5f
Lawson Hart <lawsonhart0919@gmail.com>
2026-06-05 22:07:26 -0500

    fix: lol

--- apps/web/src/lib/editor/SidebarFooter.svelte
+++ apps/web/src/lib/editor/SidebarFooter.svelte
@@ -91,7 +91,7 @@
       <circle cx="9" cy="7" r="4" />
       <path d="M23 21v-2a4 4 0 0 0-3-3.87M16 3.13a4 4 0 0 1 0 7.75" />
     </svg>
-    <span><span class="text-foreground">{userCount.toLocaleString()}</span> people use this</span>
+    <span><span class="text-foreground">{userCount.toLocaleString()}</span>+ people use this</span>
   </div>
 {/if}
 
